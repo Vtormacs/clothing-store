dede28634ca9868b20748f0765f3c12c09502b91
Vtormacs <macskids98play@hotmail.com>
2024-08-15 17:02:54 -0300

    ADD Spring Security

--- src/main/java/com/clothing_store/Entity/FuncionarioEntity.java
+++ src/main/java/com/clothing_store/Entity/FuncionarioEntity.java
@@ -1,25 +1,31 @@
 package com.clothing_store.Entity;
 
+import com.clothing_store.Entity.Role.FuncionarioRole;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode
+@EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "tb_funcionario")
-public class FuncionarioEntity {
+public class FuncionarioEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
+    private String senha;
     private String telefone;
     private Integer idade;
     private String endereco;
@@ -28,4 +34,43 @@ public class FuncionarioEntity {
     @JsonIgnore
     @OneToMany(mappedBy = "funcionario")
     private List<VendaEntity> vendas;
+
+    private FuncionarioRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == FuncionarioRole.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/clothing_store/Repository/FuncionarioRepository.java
+++ src/main/java/com/clothing_store/Repository/FuncionarioRepository.java
@@ -2,8 +2,10 @@ package com.clothing_store.Repository;
 
 import com.clothing_store.Entity.FuncionarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<FuncionarioEntity, Long> {
+    UserDetails findByEmail(String email);
 }
